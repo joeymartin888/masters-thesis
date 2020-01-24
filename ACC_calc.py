dc1da8f3a7d59e85c7429c3f4a185604d02bc69d
joeymartin888 <joeymartin888@gmail.com>
2020-01-23 17:08:50 -0800

    modest beginnings to assembling matrix for correlation

--- ACC_calc.py
+++ ACC_calc.py
@@ -10,26 +10,29 @@ import math as math
 import nc as nc
 import numpy as np
 import array as ar
-obs=nc.getvar('/home/josmarti/Data/Observations/NSIDC_1979_2010_nh_siea.nc', 'sie')
-var=nc.getvar('SIE_monthly_CanCM3_i197902.nc','sic')
-print(obs)
+obsin=nc.getvar('/home/josmarti/Data/Observations/NSIDC_1979_2010_nh_siea.nc', 'sie')
+print(obs.shape)
 print(np.mean(obs))
-print(np.mean(var*2*math.pi*6.371**2))
-#print(var)
+#print(np.mean(var*2*math.pi*6.371**2))
+#print(var.shape)
 #print(obs)
 
-f=4
-c=2
-a=6
+model="CanCM4"
+init="198812"
+#Model into ACC matrix format
+var=nc.getvar(('/home/josmarti/Data/SIE/SIE_monthly_%s_i%s.nc' % (model,init)),'sic')
+#print(var)
 
-"""
-f=ar.array('f',[1,2,3])
-c=ar.array('f',[2,3,4])
-a=ar.array('f',[3,4,5])
-"""
-
-ACCtop=np.mean(np.multiply(np.subtract(f,c),np.subtract(a,c)))
-ACCbottom=np.sqrt(np.mean(np.multiply(np.square(np.subtract(f,c)),np.square(np.subtract(a,c)))))
-ACC=np.divide(ACCtop,ACCbottom)
+#Observations into years
+for i in range(32):
+    index_pos=range(i*12,i*12+12)
+    obs=obsin[index_pos]
+    #for j in range(12):
+        #print(j)
+        
+#varem=np.mean(var, axis=1) #Average across ensembles
+#print(varem)
+#ACC=np.corrcoef(sim,obs)
 
 print(ACC)
+
